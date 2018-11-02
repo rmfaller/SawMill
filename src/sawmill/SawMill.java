7b35fbaa9eea76b9faf9e693c9588ff6df514a4a
rmfaller <robert.m.faller@gmail.com>
2018-11-01 17:36:02 -0700

    SawMill v .06

--- src/sawmill/SawMill.java
+++ src/sawmill/SawMill.java
@@ -30,6 +30,7 @@ public class SawMill {
      * example file --sla for Service Level agreement adherence --harvest needs
      * to be implemented
      * @throws java.text.ParseException
+     * @throws java.lang.InterruptedException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException, ParseException, org.json.simple.parser.ParseException, InterruptedException {
         FileReader fr = null;
@@ -57,23 +58,29 @@ public class SawMill {
         }
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
+                case "-c":
                 case "--condense":
                     br = new BufferedReader(new FileReader(args[i + 1]));
                     lf = args[i + 1];
                     cdr = new Condenser(config, br);
                     runcondense = true;
                     break;
+                case "-u":
                 case "--cut":
                     cut = Long.parseLong(args[i + 1]);
                     break;
+                case "-?":
+                case "-h":
                 case "--help":
                     help();
                     break;
-                case "--configuration":
+                case "-p":
+                case "--poi":
                     fr = new FileReader(args[i + 1]);
                     cfn = args[i + 1];
                     config = (JSONObject) jp.parse(fr);
                     break;
+                case "-l":
                 case "--laminate":
                     lbra = new BufferedReader[(args.length - i) - 1];
                     runlaminate = true;
@@ -84,7 +91,7 @@ public class SawMill {
                     }
                     lmnt = new Laminate(fra);
                     break;
-                case "--locate":
+                /*                case "--locate":
                     locatestring = args[i + 1];
                     break;
                 case "--location":
@@ -96,10 +103,12 @@ public class SawMill {
                         y++;
                     }
                     break;
-
+                 */
+                case "-t":
                 case "--totals":
                     showtotals = true;
                     break;
+                case "-s":
                 case "--sla":
                     sla = true;
                     break;
@@ -120,15 +129,26 @@ public class SawMill {
     }
 
     private static void help() {
-        String help = "\nSawMill usage:"
-                + "\njava -jar SawMill.jar --condense file_to_condense --configuration configuration/log_file_configuration.json"
-                + "\n\toptions when condensing:"
-                + "\n\t\t--totals : prints a total for each column. Do not use this option when condensing a file to be laminated."
-                + "\n\t\t--cut x  : where is x an integer and specifies the number of milliseconds used to condense the file by."
-                + "\n\t\t--sla    : includes the number of operations the meet SLAs, exceeded SLAs, and a percentage of operations that met SLAs."
-                + "\n\t\t           SLAs are assigned within the log_file_configuration."
-                + "\n\nor\n"
-                + "\njava -jar SawMill.jar --laminate condensedfile0 condensedfile1 condensedfilen..."
+        String help = "\nSawMill usage when condensing a log:"
+                + "\n\trequired for condensing:"
+                + "\n\t\t--condense | -c path and filename of log file to condense"
+                + "\n\t\t--poi      | -p path and filename of json configuration file on how to handle the log file to be condensed"
+                + "\n\tExample:"
+                + "\n\tjava -jar ./dist/SawMill.jar --condense /path/to/file_to_condense --poi /path/to/log_file_configuration.json"
+                + "\n\toptions for condensing:"
+                + "\n\t\t--totals   | -t prints a total for each column. Do not use this option when condensing a file to be laminated"
+                + "\n\t\t--cut x    | -u where is x an integer and specifies the number of milliseconds used to condense the file by"
+                + "\n\t\t--sla      | -s includes the number of operations the meet SLAs, exceeded SLAs, and a percentage of operations that met SLAs"
+                + "\n\t\t                SLAs are assigned within the log_file_configuration.json file"
+                + "\nSawMill usage when laminating together more than one condensed log file:"
+                + "\n\trequired for laminating:"
+                + "\n\t\t--laminate | -l path(s) and filename(s) of log files to laminate"
+                + "\n\tExample:"
+                + "\n\tjava -jar ./dist/SawMill.jar --laminate /path/to/condensedfile-0 /path/to/condensedfile-1 /path/to/condensedfile-n"
+                + "\n\n\tExample of condensing two log files and then laminating those files together:"
+                + "\n\t\tjava -jar ./dist/SawMill.jar --condense $HOME/openidm/audit/access.audit.json-1of2 --poi $HOME/SawMill/poi/idm5-log.json > /tmp/idm-1of2.csv"
+                + "\n\t\tjava -jar ./dist/SawMill.jar --condense $HOME/openidm/audit/access.audit.json-2of2 --poi $HOME/SawMill/poi/idm5-log.json > /tmp/idm-2of2.csv"
+                + "\n\t\tjava -jar ./dist/SawMill.jar --laminate /tmp/idm-1of2.csv /tmp/idm-2of2.csv > /tmp/idm-combined.csv"
                 + "\n";
         System.out.println(help);
     }
